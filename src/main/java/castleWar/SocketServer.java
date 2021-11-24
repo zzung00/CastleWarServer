d8b6ec8991af5bc0d9a138a20addbd116b55ee92
zzung00 <lee759444@gmail.com>
2021-11-24 20:08:17 +0900

    Initial commit

---
+++ src/main/java/castleWar/SocketServer.java
@@ -0,0 +1,15 @@
+package castleWar;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServer {
+    public static void main(String[] args) {
+        SocketServer server = new SocketServer();
+    }
+
+    public void startServer() {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+    }
+}
